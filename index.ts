3234ab37d595686b5ac52a43e9491e020119f342
HardikB <49999606+HardikVB@users.noreply.github.com>
2024-06-12 21:03:59 +0100

    Https and SSL CA

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-import { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
+
 import { router as storeRoute } from './src/routes/store';
 import { router as loginRoute } from './src/routes/login';
 import { router as registerRoute } from './src/routes/register';
+
 const app = express();
 
 // Increase the request body size limit
 app.use(bodyParser.json({ limit: '50mb' }));
-
-app.use(cors());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 interface LanguageRequest extends Request {
@@ -23,10 +24,10 @@ interface LanguageRequest extends Request {
 // Middleware to extract language parameter and pass it to the request
 app.use('/:language', (req: LanguageRequest, res: Response, next: NextFunction) => {
   req.language = req.params.language;
-
   next();
 });
 
+// Routes
 app.use('/:language/login', loginRoute);
 app.use('/:language/store', storeRoute);
 app.use('/:language/register', registerRoute);
@@ -36,6 +37,22 @@ app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor a correr na porta ${process.env.PORT}`);
+// HTTPS server configuration
+const httpsOptions = {
+  key: fs.readFileSync('caminho/para/server.key'),
+  cert: fs.readFileSync('caminho/para/server.cert')
+};
+
+const httpsPort = 443;
+const httpsServer = https.createServer(httpsOptions, app);
+
+httpsServer.listen(httpsPort, () => {
+  console.log(`Servidor HTTPS está ouvindo na porta ${httpsPort}`);
+});
+
+// HTTP server configuration (opcional para redirecionamento HTTPS)
+const httpPort = process.env.HTTP_PORT || 80;
+
+app.listen(httpPort, () => {
+  console.log(`Servidor HTTP está ouvindo na porta ${httpPort}`);
 });
